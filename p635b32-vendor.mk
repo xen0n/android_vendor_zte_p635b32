efd10ec7143bb143d245525207a9ea16b2ada23b
Wang Xuerui <idontknw.wang@gmail.com>
2016-02-21 18:14:54 +0800

    initial commit for cm-12.1; display, wifi, bluetooth working

---
+++ p635b32-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/etc,system/etc)
+
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/zte/p635b32/vendor,system/vendor)
